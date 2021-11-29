5a6d5228ccb7ab42c0b6dab64d3bde717ba9d550
Kirihi <kiriyama.hikari@outlook.com>
2021-11-28 23:05:43 -0500

    Se realizaron varios cambios

--- sign_up/process_signup.php
+++ sign_up/process_signup.php
@@ -3,12 +3,13 @@ include("../connect.php");
 
 $nombre_paciente = $_POST['nombre_paciente'];
 $idPaciente = $_POST['idPaciente'];
+$passPaciente = $_POST['passPaciente'];
 $telefono_paciente = $_POST['telefono_paciente'];
 $poliza_seg = $_POST['poliza_seg'];
 $correo_paciente = $_POST['correo_paciente'];
 $fecha_nacto = $_POST['fecha_nacto'];
 
-$sql = "insert into Pacientes values ('$nombre_paciente', '$idPaciente', '$telefono_paciente', '$poliza_seg', '$correo_paciente', '$fecha_nacto')";
+$sql = "insert into Pacientes values ('$idPaciente', '$passPaciente', '$nombre_paciente', '$telefono_paciente', '$poliza_seg', '$correo_paciente', '$fecha_nacto')";
 $stmt = sqlsrv_query($conn, $sql);
 
 if($stmt){
