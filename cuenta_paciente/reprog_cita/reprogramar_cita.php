b200e52c29cea5799122677e3952c294f7091a08
Kirihi <kiriyama.hikari@outlook.com>
2021-11-29 16:57:52 -0500

    Se crearon las páginas de reprogramación de citas

---
+++ cuenta_paciente/reprog_cita/reprogramar_cita.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+$idCita = $_POST['idCita'];
+$fechaCita = $_POST['fechaCita'];
+$horaCita = $_POST['horaCita'];
+
+include("../../connect.php");
+
+$sql = "update Citas set fechaCita = '$fechaCita' or horaCita = '$horaCita' where idCita = '$idCita'";
+$stmt = sqlsrv_query($conn, $sql);
+
+if($stmt){
+    header("Location: ../cuenta.php");
+}
+else{
+    echo "Su cita no ha podido ser reprogramada. Intentelo nuevamente.";
+    header("refresh:5;url:reprogramar_cita.html");
+}
+?>
