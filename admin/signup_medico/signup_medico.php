e9cd0c05f62cca1a4e84f35e901302b31684d7a4
Kirihi <kiriyama.hikari@outlook.com>
2021-11-29 12:19:02 -0500

    Se crean los archivos de signup_medico

---
+++ admin/signup_medico/signup_medico.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+$idMedico = $_POST['idMedico'];
+$passMedico = $_POST['passMedico'];
+$nombreMedico = $_POST['nombreMedico'];
+$telefonoMedico = $_POST['telefonoMedico'];
+$especialidad = $_POST['especialidad'];
+$correoMedico = $_POST['correoMedico'];
+
+include("../../connect.php");
+
+$sql = "insert into Medico values ('$idMedico', '$passMedico', '$nombreMedico', '$telefonoMedico', '$especialidad', '$correoMedico')";
+$stmt = sqlsrv_query($conn, $sql);
+
+if($stmt){
+    header("Location: ../cuenta_admin.php");
+}
+else{
+    echo "El perfil del médico no pudo ser creado.";
+    header("refresh:5;url:signup_medico.html");
+}
+?>
