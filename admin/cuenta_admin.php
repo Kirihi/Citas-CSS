598550536726866d8c638532624648a35d74f360
Kirihi <kiriyama.hikari@outlook.com>
2021-11-29 16:23:47 -0500

    Se crearon 2 archivos y se modificó 1

---
+++ admin/cuenta_admin.php
@@ -0,0 +1,17 @@
+<? session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuenta de Administrador</title>
+    <nav>
+        <a href="signup_medico/signup_medico.html">Registro de médico</a>
+    </nav>
+    <a href="cerrar_sesion.php">Cerrar sesión</a>
+</head>
+<body>
+    
+</body>
+</html>
