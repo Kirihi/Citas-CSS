dbe33ba42600b3b24edf885eb5a1ec5705222e13
Kirihi <kiriyama.hikari@outlook.com>
2021-11-29 16:30:58 -0500

    Se agrega el css a la cuenta del Admin

--- admin/cuenta_admin.php
+++ admin/cuenta_admin.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Cuenta de Administrador</title>
-    <nav>
+    <nav class="menu">
         <a href="signup_medico/signup_medico.html">Registro de médico</a>
     </nav>
-    <a href="cerrar_sesion.php">Cerrar sesión</a>
+    <a href="cerrar_sesion.php" class="enlaces">Cerrar sesión</a>
 </head>
 <body>
     
