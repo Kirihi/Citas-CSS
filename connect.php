89bc2595c5e40f782dd0315e88ec4fb7e896792e
Kirihi <kiriyama.hikari@outlook.com>
2021-12-11 01:08:42 -0500

    Cambios

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $serverName = "localhost";
-$serverDB = "citas_css";
 $serverUser = "root";
 $serverPswd = "Kirigaya4";
+$serverDB = "citas_css";
 
 $conn = mysqli_connect($serverName, $serverDB, $serverUser, $serverPswd);
 ?>
