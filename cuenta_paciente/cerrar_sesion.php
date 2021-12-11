d89002afcf852015b520863bf1208d9dc7fc45aa
Kirihi <kiriyama.hikari@outlook.com>
2021-12-11 11:35:45 -0500

    Se incluyó un mensaje de cierre de sesión

--- cuenta_paciente/cerrar_sesion.php
+++ cuenta_paciente/cerrar_sesion.php
@@ -1,5 +1,17 @@
 <?php
 session_start();
 session_destroy();
-header("Location: ../index.html");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cerrar sesión</title>
+</head>
+<body>
+    <center><p>Has cerrado la sesión.</p></center>
+</body>
+</html>
+<? header("refresh:5;url:../index.html"); ?>
