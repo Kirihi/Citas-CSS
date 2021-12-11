1670056888c36fd76290464bf4e9d4edeb87ff3c
Kirihi <kiriyama.hikari@outlook.com>
2021-12-11 08:15:38 -0500

    Borrado de if

--- connect.php
+++ connect.php
@@ -3,11 +3,4 @@ $serverName = "localhost\sqlexpress";
 
 $connectionInfo = array( "Database"=>"Citas-CSS");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
-
-if( $conn ) {
-     echo "Conexión establecida.<br />";
-}else{
-     echo "Conexión no se pudo establecer.<br />";
-     die( print_r( sqlsrv_errors(), true));
-}
 ?>
