7dca69447c7e19c765cd6ffc88cdb34d484ac49a
Kirihi <kiriyama.hikari@outlook.com>
2021-12-10 23:49:56 -0500

    Cambios al script

--- script_BD.sql
+++ script_BD.sql
@@ -2,7 +2,7 @@ create database Citas_CSS
 drop database Citas_CSS
 
 create table Pacientes (idPaciente varchar(20) not null constraint PK_Pacientes_idPaciente primary key, passPaciente varchar(16) not null, nombre varchar(20) not null, telefono_paciente varchar(10) not null, poliza_seg varchar(15) not null, correoPaciente varchar(30) not null, fechaNacto date not null)
-create table Citas (idCita int not null identity(1,1), policlinica varchar(50) not null, especialidad varchar(30) not null, fechaCita date not null, horaCita time not null, idPaciente varchar(20) not null constraint Fk_Citas_idPaciente foreign key references Pacientes(idPaciente))
+create table Citas (idCita int not null auto_increment constraint Pk_Citas_idCita primary key, policlinica varchar(50) not null, especialidad varchar(30) not null, fechaCita date not null, horaCita time not null, idPaciente varchar(20) not null constraint Fk_Citas_idPaciente foreign key references Pacientes(idPaciente))
 create table Medico (idMedico varchar(20) not null constraint PK_Medico_idMedico primary key, passMedico varchar(16) not null, nombreMedico varchar(20) not null, telefonoMedico varchar(10) not null, Cargo varchar(10) not null, correoMedico varchar(30) not null)
 create table [Admin] (emailAdmin varchar(30) not null, passAdmin varchar(16) not null, nombreAdmin varchar(30) not null)
 
