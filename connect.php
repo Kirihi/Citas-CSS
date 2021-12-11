8abaa59d4b3327730f73b866153b6396853f1996
Kirihi <kiriyama.hikari@outlook.com>
2021-12-10 19:09:29 -0500

    Cambios realizados

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-$serverName = "localhost\sqlexpress";
+$serverName = "20.120.6.3";
 
-$connectionInfo = array("Database"=>"Citas_CSS");
+$connectionInfo = array("Database"=>"Citas_CSS", "UID"=>"sa", "PWD"=>"Kirigaya4");
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
